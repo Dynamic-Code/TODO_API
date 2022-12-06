8622e22a60868a0c433034fbe8fb124fdde8c28f
Abhinav Kumar <dynamic2106@outlook.com>
2022-12-06 22:49:32 +0530

    #added Register endpoint

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using API.Context;
+using API.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class UserController : BaseController
+    {
+        private ApplicationDbContext _dbContext { get; }
+
+        public UserController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Register>> Register(Register register)
+        {
+            var user = new User();
+            user.UserId = Guid.NewGuid();
+            user.UserName = register.UserName;
+            user.Password = register.Password;
+            user.Email = register.Email;
+
+
+            _dbContext.Add(user);
+            await _dbContext.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
